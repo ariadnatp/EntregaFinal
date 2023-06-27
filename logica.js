106c92576ce99d4bfcfbc78aabb09245fb0f1707
Ariadna Pellejero <pellejeroariadna@gmail.com>
2023-06-27 10:28:23 -0300

    consulta

--- logica.js
+++ logica.js
@@ -8,8 +8,8 @@ let contenedorProds = document.getElementById("misprods");
     contenedorProds.innerHTML=" "; //vacio contenedor
     for (const producto of listaProds) {
       contenedorProds.innerHTML+=`
-          <div class="card col-md-5">
-              <img class="card-img-top" src="${producto.foto}" alt="${producto.nombre}" style="width: 90px">
+          <div class="card col-md-5 center">
+              <img class="card-img-top rounded mx-auto d-block" src="${producto.foto}" alt="${producto.nombre}" style="width: 170px">
               <div class="card-body">
                   <h5 class="card-title">${producto.nombre}</h5>
                   <p class="card-text">$ ${producto.precio}</p>
@@ -42,7 +42,7 @@ for(const boton of botones){
 
 function agregarACarro(producto){
   carro.push(producto);
-  console.table(carro);
+  console.table(producto);
   tableBody.innerHTML += `
   <tr>
       <td>${producto.id}</td>
@@ -50,8 +50,19 @@ function agregarACarro(producto){
       <td>${producto.precio}</td>
   </tr>
 `;
+
+let totalPrecio = productos.reduce((acumular, producto)=> acumular + producto.precio,0);
+console.log(totalPrecio);
+document.getElementById("total").innerText = "Total a pagar: $ " +totalPrecio;
+localStorage.setItem('carro',JSON.stringify(carro));
+
 }
 
+// let total = carro.reduce((ac,prod)=> ac + prod.precio,0);
+//     console.log(total);
+//     document.getElementById('total').innerText = `Total a pagar $:${total}`;
+//     //trabajar con el storage
+//     localStorage.setItem('carro',JSON.stringify(carro));
 
 
 
